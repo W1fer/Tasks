36998d2b4a69e7795e5a5ac557adef41c5f2543d
W1fer <95349758+W1fer@users.noreply.github.com>
2022-02-23 17:04:56 +0300

    Delete main.py

--- Work/main.py
+++
@@ -1,8 +0,0 @@
-n = input()
-
-if n.count('(') > n.count(')'):
-    print('Имеется не закрытая скобка ')
-elif n.count('(') < n.count(')'):
-    print('Лишняя закрывающая скобка ')
-else:
-    print('Все Ок')
