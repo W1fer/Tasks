7e16ebd6e21ef9fcf83118e845dc92ae5b0d25e5
W1fer <95349758+W1fer@users.noreply.github.com>
2022-02-28 18:15:46 +0300

    Add files via upload

---
+++ Task_2/main/php/config.php
@@ -0,0 +1,35 @@
+<?
+	// *** Настройка обязательности полей, в случае если они присутствуют в вашей форме
+
+    // Текстовое поле
+
+    const TEXTISREQUIRED = false;
+	const MSGSTEXTERROR = "⚠ Поле обязательно для заполнения";
+
+	// ФИО
+	const N_S_PISREQUIRED = true;
+	const MSGSNAMEERROR = "⚠ Поле обязательно для заполнения";
+
+	// Адрес
+	const ADDRESSISREQUIRED = false;
+	const MSGSADDRESSERROR = "⚠ Поле обязательно для заполнения";
+
+	// Email
+	const EMAILISREQUIRED = false;
+	const MSGSEMAILERROR = "⚠ Поле обязательно для заполнения";
+	const MSGSEMAILINCORRECT = "Почтовые адреса введён верно";
+	const MSGEMAILBLOCKED="Почтовый адрес @gmail.com не поддерживается "
+
+	// Телефон
+	const TELISREQUIRED = true;
+	const MSGSTELERROR = "⚠ Поле обязательно для заполнения";
+
+	// Файл
+	const FILEISREQUIRED = false;
+	const MSGSFILEERROR = "⚠ Поле обязательно для заполнения";
+
+
+
+
+
+
