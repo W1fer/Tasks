f769ed5c32b9c313fff410f7cea9f34f60cb30cb
W1fer <95349758+W1fer@users.noreply.github.com>
2022-02-28 13:16:30 +0300

    Update main.py

--- Task_1/main.py
+++ Task_1/main.py
@@ -1,8 +1,11 @@
 n = input()
 
-if n.count('(') > n.count(')'):
-    print('Имеется не закрытая скобка ')
+if n.find(')') < n.find('('):
+    print('неверная последовательность скобок ')
+elif n.count('(') > n.count(')'):
+    print(f'Имеется не закрытая скобка ')
 elif n.count('(') < n.count(')'):
     print('Лишняя закрывающая скобка ')
 else:
     print('Все Ок')
+
